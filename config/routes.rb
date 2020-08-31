d509148f3745eed2a3d8128c37d60a9e261d218f
kohey127 <koooochoki@gmail.com>
2020-09-01 04:10:50 +0900

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :owners, controller: {
+    sessions: "models/sessions",
+    passwords: "models/passwords",
+    registrations: "models/registration"
+    }
+
+  devise_for :customers, controller: {
+    sessions: "models/sessions",
+    passwords: "models/passwords",
+    registrations: "models/registration"
+    }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
