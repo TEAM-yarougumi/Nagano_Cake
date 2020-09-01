bb2dcca858107249d04670c942edad50c8eed7d4
山本　薫 <>
2020-09-01 23:46:08 +0900

    generate ec controller

---
+++ app/controllers/customer/orders_controller.rb
@@ -0,0 +1,23 @@
+class Customer::OrdersController < ApplicationController
+
+    def index
+    end
+
+    def create
+    end
+
+    def thanks
+    end
+
+    def show
+    end
+
+    def new
+    end
+
+    def confirm
+    end
+    
+    def complete
+    end
+end
