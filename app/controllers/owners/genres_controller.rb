1be10bc69113554096187f9416ec6d52c56d49aa
山本　薫 <>
2020-09-02 16:18:50 +0900

    add genre form

--- app/controllers/owners/genres_controller.rb
+++ app/controllers/owners/genres_controller.rb
@@ -1,9 +1,14 @@
 class Owners::GenresController < ApplicationController
 
 def index
+     @genres = Genre.all 
 end
 
 def create
+    @genre = Genre.new(genre_params) 
+    if @genre.save
+    redirect_to owners_genres_path
+    end
 end
 
 def edit
@@ -12,4 +17,9 @@ end
 def update
 end
 
+
+private
+def genre_params
+    params.permit(:name, :status)
+  end
 end
