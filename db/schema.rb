7abdce661267c03d9648d6f92dc320cab2eb41a2
kohey127 <koooochoki@gmail.com>
2020-09-02 17:02:47 +0900

    temp_push

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_081151) do
+ActiveRecord::Schema.define(version: 2020_09_01_082525) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "item_id"
@@ -42,9 +42,9 @@ ActiveRecord::Schema.define(version: 2020_09_01_081151) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.boolean "status", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "status", default: false, null: false
   end
 
   create_table "items", force: :cascade do |t|
