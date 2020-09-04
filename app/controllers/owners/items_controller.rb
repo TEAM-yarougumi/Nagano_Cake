fceb9523882d435a81d40728f899a5c032586c3c
山本　薫 <>
2020-09-04 14:56:30 +0900

    new itempage1

--- app/controllers/owners/items_controller.rb
+++ app/controllers/owners/items_controller.rb
@@ -1,13 +1,22 @@
 class Owners::ItemsController < ApplicationController
-  before_action :authenticate_owner!
+ # before_action :authenticate_owner! 一時的に無効化
 
 def new
+  @item = Item.new
 end
 
 def create
+  @item = Item.new(item_params)
+  if @item.save
+    redirect_to owners_items_index_owner_path 
+  else 
+    render  'new'
+  end
 end
 
 def index_owner
+  @item = Item.all
+
 end
 
 def show_owner
@@ -19,4 +28,10 @@ end
 def update
 end
 
+
+private
+  def item_params
+    params.require(:item).permit(:name, :content, :no_tax_price, :sale_status, :image ,:genre_id)
+  end
 end
+
