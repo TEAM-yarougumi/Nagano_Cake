eba558597b2541e1b2d22fd9444c3523d457bbd2
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-09-05 17:30:51 +0900

    add_ec_shipping_address_index

--- app/controllers/customers/shipping_addresses_controller.rb
+++ app/controllers/customers/shipping_addresses_controller.rb
@@ -2,12 +2,25 @@ class Customers::ShippingAddressesController < ApplicationController
   before_action :authenticate_customer!
 
     def index
+        @shipping_address = ShippingAddress.new
+        @shipping_addresses = ShippingAddress.all
     end
 
     def create
+        @shipping_address = ShippingAddress.new(shipping_address_params)
+        @shipping_address.customer_id = current_customer.id
+
+        if @shipping_address.save
+            redirect_to customer_shipping_addresses_path
+        else
+            render "index"
+        end
     end
 
     def destroy
+        @shipping_address = ShippingAddress.find(params[:id])
+        @shipping_address.destroy
+        redirect_to customer_shipping_addresses_path
     end
 
     def edit
@@ -18,7 +31,7 @@ class Customers::ShippingAddressesController < ApplicationController
 
 
     private
-  def shipping_addresses_params
-    params.require(:shipping_addresses).permit(:address_name,:shipping_address,:shipping_postal_code)
-  end
+    def shipping_address_params
+        params.require(:shipping_address).permit(:address_name,:shipping_address,:shipping_postal_code)
+    end
 end
