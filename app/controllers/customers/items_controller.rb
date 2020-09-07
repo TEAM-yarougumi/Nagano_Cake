c4e63aba1f51aaf949e684ce23e024e07eae2ed0
kohey127 <koooochoki@gmail.com>
2020-09-07 15:59:29 +0900

    Add_view

--- app/controllers/customers/items_controller.rb
+++ app/controllers/customers/items_controller.rb
@@ -1,10 +1,27 @@
 class Customers::ItemsController < ApplicationController
-    def index
-
+  def index
+    binding.pry
+    if request.query_string == ""
+      # 商品一覧
+      @genres = Genre.where(status: true)
+      @items = Item.page(params[:page]).reverse_order
+      @title = "商品"
+    else
+      # ジャンル一覧
+      @genres = Genre.where(status: true)
+      genre = Genre.find(params[:genre_nav])
+      items = Item.where(genre_id: genre.id)
+      @items = items.page(params[:page]).reverse_order
+      @title = genre.name
     end
+  end
 
-    def show
-        
-    end
-    
+  def search
+
+  end
+
+  def show
+
+  end
+  
 end
