93ba34a06830842a62e112b9c69eb729fec4e477
kohey127 <koooochoki@gmail.com>
2020-09-11 13:07:44 +0900

    TempCommit

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -1,69 +1,53 @@
 class Customers::OrdersController < ApplicationController
-    before_action :authenticate_customer!
+	before_action :authenticate_customer!
 
-    def index
-    end
+	def index
+	end
 
-    def create
-        @order =Order.new(order_params)
-    end
+	def create
+		@order =Order.new(order_params)
+	end
 
-    def thanks
-    end
+	def thanks
+	end
 
-    def show
-    end
+	def show
+	end
 
-    def new
-        @order = Order.new
-        @customer = Customer.find(params[:customer_id])
-        @shipping_addresses = ShippingAddress.all
-    end
+	def new
+		@order = Order.new
+		@customer = Customer.find(params[:customer_id])
+		@shipping_addresses = ShippingAddress.all
+	end
 
-    def confirm
-        if params[:choice_shipping_address] == "my_address"
-            @customer = Customer.find(params[:customer_id])
-            @order = Order.new
-            @order.postal_code = @customer.postal_code
-            @order.address = @customer.address
-            @order.address_name = @customer.last_name, @customer.first_name
+	def confirm
+    @order = Order.new
+		if params[:choice_shipping_address] == "my_address"
+			@order.postal_code = current_customer.postal_code
+			@order.address = current_customer.address
+			@order.address_name = current_customer.last_name, current_customer.first_name
+		elsif params[:choice_shipping_address] == "registered_address"
+			@shipping_address = ShippingAddress.find(params[:choice_pulldown])
+			@order.postal_code = @shipping_address.shipping_postal_code
+			@order.address = @shipping_address.shipping_address
+			@order.address_name = @shipping_address.address_name
+		else
+			@order.postal_code = params[:postal_code]
+			@order.address = params[:address]
+			@order.address_name = params[:address_name]
+		end
+    @cart_items = CartItem.all
+    @cart_item = CartItem.find(params[:id])
+    @cart_item_tax_price =  (@cart_item.item.no_tax_price * 1.1).floor
+    @cart_items_total_price = @cart_item_tax_price * @cart_item.number.sum
+	end
+	
+	def complete
+	end
 
-            @cart_items = CartItem.all
-            @cart_item = CartItem.find(params[:id])
-            @cart_item_tax_price =  (@cart_item.item.no_tax_price * 1.1).floor
-            @cart_items_total_price = @cart_item_tax_price * @cart_item.number.sum
-
-        elsif params[:choice_shipping_address] == "registered_address"
-            @order = Order.new
-            @shipping_address = ShippingAddress.find(params[:choice_pulldown])
-            @order.postal_code = @shipping_address.shipping_postal_code
-            @order.address = @shipping_address.shipping_address
-            @order.address_name = @shipping_address.address_name
-
-            @cart_items = CartItem.all
-            @cart_item = CartItem.find(params[:id])
-            @cart_item_tax_price =  (@cart_item.item.no_tax_price * 1.1).floor
-            @cart_items_total_price = @cart_item_tax_price * @cart_item.number.sum
-
-        else
-            @order = Order.new
-            @order.postal_code = params[:postal_code]
-            @order.address = params[:address]
-            @order.address_name = params[:address_name]
-
-            @cart_items = CartItem.all
-            @cart_item = CartItem.find(params[:id])
-            @cart_item_tax_price =  (@cart_item.item.no_tax_price * 1.1).floor
-            @cart_items_total_price = @cart_item_tax_price * @cart_item.number.sum
-        end
-    end
-    
-    def complete
-    end
-
-    private
-    def order_params
-        params.require(:order).permit(:payment, :address_name, :address, :postal_code)
-        
-    end
+	private
+	def order_params
+		params.require(:order).permit(:payment, :address_name, :address, :postal_code)
+		
+	end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :customer 
   has_many :order_items, dependent: :destroy
+  has_many :items, through: :order_items # 中間テーブルのアソシエーション定義
 
   validates :address, presence: true
   validates :address_name, presence: true
   validates :postal_code, presence: true
-      
+
   enum payment: { credit_card: 0, bank_transfer: 1 }
   validates :payment, inclusion: { in: Order.payments.keys }
 
