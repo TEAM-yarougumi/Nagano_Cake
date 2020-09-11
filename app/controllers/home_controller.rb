3d1aa52f71bea5fec75aaec566cc8e3f511a3139
vagrant <vagrant@localhost.localdomain>
2020-09-11 16:49:46 +0000

    test

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     @genres = Genre.where(status: true)
     @items = Item.find(1, 2, 3, 4)
   end
-  
+
 	def about
 	end
 end
--- app/controllers/owners/orders_controller.rb
+++ app/controllers/owners/orders_controller.rb
@@ -1,18 +1,32 @@
 class Owners::OrdersController < ApplicationController
   before_action :authenticate_owner!
 
-	def index
-		@orders = Order.all.page(params[:page]).per(10)
-	end
+  def index
+  	   @orders = Order.page(params[:page]).per(3).order('created_at DESC')
+  end
 
-	def show
-	end
+  def show
+  	   @order = Order.find(params[:id])
+  end
 
-	def update
-	end
+  def update
+       @order = Order.find(params[:id])
+       @order.update(orders_params)
+       @orderitems = @order.order_items.all
+      if @order.orders_status == "deposit"
+        @orderitems.each do |order_item|
+        order_item.update(production_status: :waiting)
+         end
+        redirect_to admins_order_path(@order.id)
+      else
+        render :show
+      end
+  end
 
-	private
-	def order_status
-	    params.require(:order).permit(:order_status)
-	end
+  private
+
+  def orders_params
+    params.require(:order).permit(:orders_status)
+  end
+
 end
