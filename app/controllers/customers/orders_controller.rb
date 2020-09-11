195923d0f1efcd2c24c0813e0e37b228f5c255d0
YutoSuzuki1024 <yuto.suzuki1024@gmail.com>
2020-09-11 18:28:17 +0900

    update_ec_order_confirm

--- app/controllers/customers/orders_controller.rb
+++ app/controllers/customers/orders_controller.rb
@@ -5,35 +5,59 @@ class Customers::OrdersController < ApplicationController
   end
 
   def create
-    binding.pry
-    @order = Order.new(order_params)
-    if params[:order][:choice_shipping_address] == "my_address"
+    if current_customer.cart_items.exists?
+      #お届け先によって保存内容を場合分け
+      @order = Order.new(order_params)
       @order.customer_ID = current_customer.id
-      @order.postal_code = current_customer.postal_code
-      @order.address = current_customer.address
-      @order.address_name = current_customer.last_name + current_customer.first_name
-      @order.billing_amount = params[:order][:billing_amount]
-    elsif params[:order][:choice_shipping_address] == "registered_address"
-      @order.customer_ID = current_customer.id
-      @shipping_address = ShippingAddress.find(params[:choice_pulldown])
-      @order.postal_code = @shipping_address.shipping_postal_code
-      @order.address = @shipping_address.shipping_address
-      @order.address_name = @shipping_address.address_name
-      @order.billing_amount = params[:order][:billing_amount]
-    else
-      @order.customer_ID = current_customer.id
-      @order.postal_code = params[:postal_code]
-      @order.address = params[:address]
-      @order.address_name = params[:address_name]
-      @order.billing_amount = params[:order][:billing_amount]
-    end
 
-    binding.pry
-    if @order.save
+      if params[:order][:choice_shipping_address] == "my_address"
+        @order.postal_code = params[:order][:postal_code]
+        @order.address = params[:order][:address]
+        @order.address_name = params[:order][:address_name]
+        @order.billing_amount = params[:order][:billing_amount]
+
+      elsif params[:order][:choice_shipping_address] == "registered_address"
+        @order.postal_code = params[:order][:postal_code]
+        @order.address = params[:order][:address]
+        @order.address_name = params[:order][:address_name]
+        @order.billing_amount = params[:order][:billing_amount]
+
+      else
+        @order.postal_code = params[:order][:postal_code]
+        @order.address = params[:order][:address]
+        @order.address_name = params[:order][:address_name]
+        @order.billing_amount = params[:order][:billing_amount]
+      end
+      
+      @order.save
+
+      #今回使われた配送先が新規のものであれば、配送先に新規登録
+      if ShippingAddress.find_by(shipping_address: @order.address).nil?
+        @shipping_address =ShippingAddress.new
+        @shipping_address.shipping_postal_code = @order.postal_code
+        @shipping_address.shipping_address = @order.address
+        @shipping_address.address_name = @order.address_name
+        @shipping_address.customer_id = current_customer.id
+        @shipping_address.save
+      end
+
+      #cart_itemsの内容をorder_itemsに新規登録
+      current_customer.cart_items.each do |cart_item|
+        order_item = @order.order_items.build
+        order_item.order_id = @order.id
+        order_item.items_id = cart_item.item_id
+        order_item.number = cart_item.number
+        order_item.buy_price = (cart_item.item.no_tax_price * 1.1).floor
+        binding.pry
+        order_item.save
+        cart_item.destroy #order_itemsに新規登録後はカートの中を空にする
+      end
       redirect_to complete_path(current_customer.id, @order.id)
-    else
-      render 'root'
-    end
+
+      else
+        redirect_to root_path
+        flash[:danger] = "カートが空です。"
+      end
   end
 
   def thanks
@@ -49,30 +73,28 @@ class Customers::OrdersController < ApplicationController
   end
 
   def confirm
-    binding.pry
+    #注文情報入力画面でのお届け先選択によって分岐
+    @order = Order.new
     if params[:choice_shipping_address] == "my_address"
       @customer = Customer.find(params[:customer_id])
-      @order = Order.new
       @order.postal_code = @customer.postal_code
       @order.address = @customer.address
       @order.address_name = @customer.last_name + @customer.first_name
-      @choice_shipping_address = params[:choice_shipping_address]
-      @cart_items = CartItem.all
+      @cart_items = current_customer.cart_items
 
     elsif params[:choice_shipping_address] == "registered_address"
-      @order = Order.new
       @shipping_address = ShippingAddress.find(params[:choice_pulldown])
       @order.postal_code = @shipping_address.shipping_postal_code
       @order.address = @shipping_address.shipping_address
       @order.address_name = @shipping_address.address_name
-      @choice_shipping_address = params[:choice_shipping_address]
-      @cart_items = CartItem.all
-      
+      @cart_items = current_customer.cart_items
 
     else
-      @order = Order.new(order_params)
-      @choice_shipping_address = params[:choice_shipping_address]
-      @cart_items = CartItem.all
+      binding.pry
+      @order.postal_code = params[:postal_code]
+      @order.address = params[:address]
+      @order.address_name = params[:address_name]
+      @cart_items = current_customer.cart_items
     end
   end
   
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,7 @@
 class OrderItem < ApplicationRecord
 	belongs_to :order
-	belongs_to :item
-	validates :number, presense: true
-	enum item_status: { 着手不可: 0, 製作待ち: 1, 製作中: 2, 製作完了: 3 }
+	belongs_to :item, optional: true
+	validates :number, presence: true
+	enum item_status: { not_start: 0, standby_making: 1, making: 2, making_finish: 3 }
+	validates :item_status, inclusion: { in: OrderItem.item_statuses.keys }
 end
