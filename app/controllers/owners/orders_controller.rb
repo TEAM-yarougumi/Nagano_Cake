28ac4749bea0df8182fd8fca003d6fc7db5b650e
山本　薫 <>
2020-09-12 22:10:57 +0900

    git uzaiyo

--- app/controllers/owners/orders_controller.rb
+++ app/controllers/owners/orders_controller.rb
@@ -19,11 +19,19 @@ class Owners::OrdersController < ApplicationController
 			flash[:notice] = "更新しました！"
 			redirect_to owners_orders_path
 			end	
-
+	def update_order
+		@order_item = OrderItem.find(params[:id])
+		@order_item.update(order_items_params)
+			if @order_item.save
+				flash[:notice] = "更新しました！"
+				redirect_to owners_orders_path
+			end	
 	end
 
 	private
 	def orders_params
-	    params.require(:order).permit(:customer_id, :postage, :billing_amount, :payment, :address, :postal_code, :status, :address_name,)
+		params.require(:order).permit(:customer_id, :postage, :billing_amount, :payment, :address, :postal_code, :status, :address_name )
 	end
+	def order_items_params
+		  params.require(:order_item).permit(:item_status)
 end
