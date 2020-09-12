423f10745a424d7f8d1264ca39fb82f2d95ea1ee
山本　薫 <>
2020-09-12 11:24:59 +0900

    muzui

--- app/controllers/owners/orders_controller.rb
+++ app/controllers/owners/orders_controller.rb
@@ -2,17 +2,25 @@ class Owners::OrdersController < ApplicationController
   before_action :authenticate_owner!
 
 	def index
+		@order = Order.all
 		@orders = Order.all.page(params[:page]).per(10)
 	end
 
 	def show
+		@order = Order.find(params[:id])
 	end
 
 	def update
+		@order = Order.find(params[:id])
+		@order.update(orders_params)
+			if @order.save
+			flash[:notice] = "更新しました！"
+			redirect_to owners_orders_path
+			end	
 	end
 
 	private
-	def order_status
-	    params.require(:order).permit(:order_status)
+	def orders_params
+	    params.require(:order).permit(:customer_id, :postage, :billing_amount, :payment, :address, :postal_code, :status, :address_name)
 	end
 end
