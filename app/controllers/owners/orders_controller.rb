6b2ec0aaf16b63d99ecc3b0fe80aed43bb18a7ee
山本　薫 <>
2020-09-13 14:32:39 +0900

    amoung order_showpage

--- app/controllers/owners/orders_controller.rb
+++ app/controllers/owners/orders_controller.rb
@@ -13,7 +13,6 @@ class Owners::OrdersController < ApplicationController
 
 	def update
 		@order = Order.find(params[:id])
-
 		
 		if @order.update(orders_params)
 			flash[:notice] = "更新しました！"
@@ -21,25 +20,33 @@ class Owners::OrdersController < ApplicationController
 				@order.order_items.each do |order_item|
 					order_item.standby_making!
 				end	
-			end
+		  end
+			redirect_to owners_orders_path
 		end
-		redirect_to owners_orders_path
 	end
 
-		def update_order
-			@order_item = OrderItem.find(params[:id])
-			@order_item.update(order_items_params)
-			if @order_item.save
-				flash[:notice] = "更新しました！"
-				redirect_to owners_orders_path
-			end	
-		end
+	def update_order
+		@order = Order.find(params[:order_item][:order_id])
+		@order_item = OrderItem.find(params[:id])
+		@order_items = OrderItem.where(order_id: params[:order_item][:order_id])
+		if @order_item.update(order_items_params)
+			flash[:notice] = "更新しました！"
+			if @order_item.item_status == "making"
+				@order.making!
+			end
+			binding.pry
+			if @order_items.item_status  == "making_finish"
+				@order.ready_to_ship!
+			end
+			redirect_to owners_orders_path
+		end	
+	end
 
 	private
 	def orders_params
 	    params.require(:order).permit(:customer_id, :postage, :billing_amount, :payment, :address, :postal_code, :status, :address_name )
 	end
 	def order_items_params
-		params.require(:order_item).permit(:item_status)
+		  params.require(:order_item).permit(:item_status)
 	end
 end
