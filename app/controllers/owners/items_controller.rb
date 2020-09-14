1c3c134b9ff8e4ecc2fdd58fd6ec0995a8ab3db9
kohey127 <koooochoki@gmail.com>
2020-09-14 15:50:38 +0900

    overwritten_by_remote_develop

--- app/controllers/owners/items_controller.rb
+++ app/controllers/owners/items_controller.rb
@@ -1,10 +1,9 @@
 class Owners::ItemsController < ApplicationController
- before_action :authenticate_owner! 一時的に無効化
+  before_action :authenticate_owner! 一時的に無効化
 
 def new
   @item = Item.new
   @genres = Genre.where(status: true)
-
 end
 
 def create
